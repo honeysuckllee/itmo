99e75187c5ce2552fc8c291f6a56be79d8b71813
honeysuckle <xaxulinasv@gmail.com>
2025-08-08 02:38:22 +0300

    Update Server.java

--- 1_course/2_sem/proga/lab7/Server6/src/main/java/ru/lab7/Server.java
+++ 1_course/2_sem/proga/lab7/Server6/src/main/java/ru/lab7/Server.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class Server {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-        ServerApp server = new ServerApp(ConfigReader.getPortFromConfig("C:\\Users\\Светлана\\IdeaProjects\\lab7\\Server6\\src\\main\\java\\ru\\lab7\\Service\\config.json"));
+        ServerApp server = new ServerApp(ConfigReader.getPortFromConfig("..\\config.json"));
         server.run();
     }
 }
